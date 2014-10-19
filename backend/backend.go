57bfad4aeef57d5375c4cef789c60952fbbd1778
Kelsey Hightower <kelsey.hightower@gmail.com>
2014-10-19 01:21:45 -0700

    add comments

--- backend/backend.go
+++ backend/backend.go
@@ -1,12 +1,20 @@
 package backend
 
+// Response represents a response from a backend store.
 type Response struct {
 	Value []byte
 	Error error
 }
 
+// A Store is a K/V store backend that retrieves and sets, and monitors
+// data in a K/V store.
 type Store interface {
+	// Get retrieves a value from a K/V store for the provided key.
 	Get(key string) ([]byte, error)
+
+	// Set sets the provided key to value.
 	Set(key string, value []byte) error
+
+	// Watch monitors a K/V store for changes to key.
 	Watch(key string, stop chan bool) <-chan *Response
 }
